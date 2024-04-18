f072b1f519c645751b257a0768f15347d50146ad
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-04-18 14:59:09 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-'''
+
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+'''
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 '''
 
@@ -31,7 +32,7 @@ except Exception as e:
 
 
 
-'''
+
 STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
@@ -45,7 +46,7 @@ except Exception as e:
 
 
 
-
+'''
 STAGE_NAME = "Evaluation stage"
 try:
    logger.info(f"*******************")
