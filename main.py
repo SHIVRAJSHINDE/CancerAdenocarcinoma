9a33f7eb64d3d1bdab9abeaeb60b685a1e7f195b
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-04-18 13:18:24 +0530

    Base Model Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-'''from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+'''
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 '''
@@ -17,10 +18,7 @@ except Exception as e:
     raise e
 
 
-
-
-
-'''STAGE_NAME = "Prepare base model"
+STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -33,7 +31,7 @@ except Exception as e:
 
 
 
-
+'''
 STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
